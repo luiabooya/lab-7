7076d911cddbb425f6e12eb62c0d4b4987086948
profh <profh@cmu.edu>
2014-02-14 13:40:15 -0500

    Base code for starting the BookManager 2 lab (lab 6)

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+BookManager2::Application.routes.draw do
+  # Authentication routes
+  get 'user/edit' => 'users#edit', :as => :edit_user
+  get 'signup' => 'users#new', :as => :signup
+  get 'logout' => 'sessions#destroy', :as => :logout
+  get 'login' => 'sessions#new', :as => :login
+  resources :sessions
+  resources :users
+  
+  # Generated model routes
+  resources :authors
+  resources :categories
+  resources :books
+  
+  # Additional routes
+  get 'proposed' => 'books#proposed', as: :proposed_books
+  get 'contracted' => 'books#contracted', as: :contracted_books
+  
+  # Set the root url
+  root to: 'books#index'
+
+end
